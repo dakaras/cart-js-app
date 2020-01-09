4019132d495f0f4b7d9d157762e70869426a4918
dakaras <dan.karagiannis@protonmail.ch>
2020-01-09 16:00:32 -0600

    added getProducts

--- app.js
+++ app.js
@@ -9,3 +9,33 @@ const cartItems = document.querySelector('.cart-items')
 const cartTotal = document.querySelector('.cart-total')
 const cartContent = document.querySelector('.cart-content')
 const productsDOM = document.querySelector('.products-center')
+
+//cart 
+let cart = []
+
+//getting products
+class Products {
+    async getProducts() {
+        try {
+            let result = await fetch('products.json')
+            return result
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+//display products
+class UI {
+
+}
+
+//local storage
+class Storage {
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const ui = new UI()
+    const products = new Products()
+})
